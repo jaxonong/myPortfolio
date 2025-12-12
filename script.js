82405b11893261697f56dcbea5b408106b298900
jaxonong <xoffee420@gmail.com>
2025-12-12 15:01:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ document.querySelectorAll("a[href^='#']").forEach(anchor => {
     });
   });
 });
+
+const fadeElements = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+fadeElements.forEach(el => observer.observe(el));
