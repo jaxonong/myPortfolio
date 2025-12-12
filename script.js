6909eb329d0b1c778c4282c0a28989cc98cca2a3
jaxonong <xoffee420@gmail.com>
2025-12-12 15:00:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const btn = document.getElementById("darkModeToggle");
 btn.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 });
+
+document.querySelectorAll("a[href^='#']").forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
